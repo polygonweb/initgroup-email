0f86b047c254bbc6739f849bb2134772ba87368d
Monochromer <monochromer@mail.ru>
2016-09-22 23:44:25 +0500

    Инициализация репозитория

---
+++ send.js
@@ -0,0 +1,26 @@
+var nodemailer = require('nodemailer');
+var pug = require('pug');
+
+var compiledFunction = pug.compileFile('src/index.pug');
+var data = JSON.parse(require('data.json'));
+var html = compiledFunction(data);
+
+var emailSettings = require('emailSettings.js');
+
+transporter = nodemailer.createTransport(emailSettings.transportInfo);
+
+var mailOptions = {
+	from: emailSettings.auth.user,
+	to: emailSettings.recipients,
+	subject: 'Email Test',
+	html: html
+};
+
+function sendHandler(error, res) {
+	if(error) {
+		return console.log(error);
+	}
+	console.log('Message sent: ' + res.response);
+};
+
+transporter.sendMail(mailOptions, sendHandler);
